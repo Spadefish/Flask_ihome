a025cf60e02f8374b7e59a4aa3f28ed788f1ce2b
xingchenhuang <xingchenhuang@webot.co>
2019-07-10 15:03:38 +0800

    完场数据库迁移脚本配置

--- ihome/api_1_0/__init__.py
+++ ihome/api_1_0/__init__.py
@@ -1,9 +1,8 @@
 # coding:utf-8
 
 from flask import Blueprint
+# 导入蓝图的视图函数
+from . import demo
 
 # 创建蓝图对象
 api = Blueprint('api_1_0', __name__)
-
-# 导入蓝图的视图函数
-from . import demo
--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 # coding:utf-8
 
-from ihome import create_app
+from ihome import create_app, db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 # 创建flask应用对象
 app = create_app('develop')
 
+manager = Manager(app)
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()

c2358b1027e5cd21dc139d0e7539f0463bc69555
xingchenhuang <xingchenhuang@webot.co>
2019-07-11 17:24:09 +0800

    添加csrf_token防护逻辑

--- ihome/web_html.py
+++ ihome/web_html.py
@@ -1,6 +1,7 @@
 # coding:utf-8
 
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response
+from flask_wtf import csrf
 
 # 提供静态文件蓝图
 html = Blueprint('web_html', __name__)
@@ -16,5 +17,14 @@ def get_html(html_file_name):
     # 过滤资源名为favicon.ico
     if html_file_name != 'favicon.co':
         html_file_name = 'html/' + html_file_name
+
+    # 创建一个csrf_token的值
+    csrf_token = csrf.generate_csrf()
+
     # flask提供的返回静态文件的方法
-    return current_app.send_static_file(html_file_name)
+    resp = make_response(current_app.send_static_file(html_file_name))
+
+    # 在创建的响应对象里面添加cookie的值
+    resp.set_cookie('csrf_token', csrf_token)
+
+    return resp

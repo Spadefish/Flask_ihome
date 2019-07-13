4cad52e05ce5d17589b6637da01fd605df089f36
spadefish <793378953@qq.com>
2019-07-13 11:09:43 +0800

    update .gitignore

--- ihome/static/js/ihome/newhouse.js
+++
@@ -1,9 +0,0 @@
-function getCookie(name) {
-    var r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
-    return r ? r[1] : undefined;
-}
-
-$(document).ready(function(){
-    // $('.popup_con').fadeIn('fast');
-    // $('.popup_con').fadeOut('fast');
-})
--- ihome/static/static/js/ihome/auth.js
+++
@@ -1,8 +0,0 @@
-function showSuccessMsg() {
-    $('.popup_con').fadeIn('fast', function() {
-        setTimeout(function(){
-            $('.popup_con').fadeOut('fast',function(){}); 
-        },1000) 
-    });
-}
-
